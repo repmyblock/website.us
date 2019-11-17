990dc112c418f80cc80456761431a848ec8f1862
root <root@redirect-01.outrageddems.nyc>
2019-11-17 20:19:17 +0000

    First Commit of the National Website

---
+++ htdocs/index.php
@@ -0,0 +1,429 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" lang="en-US" prefix="og: http://ogp.me/ns#">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" lang="en-US" prefix="og: http://ogp.me/ns#">
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html lang="en-US" prefix="og: http://ogp.me/ns#">
+<!--<![endif]-->
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Rep My Block - Rep My Block</title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="https://repmyblock.us/xmlrpc.php">
+
+	<link rel="stylesheet" type="text/css" href="/font/montserrat.css">
+	<link rel="stylesheet" type="text/css" href="/css/RepMyBlock.css">		
+
+	<!--[if lt IE 9]>
+	<script src="https://repmyblock.nyc/wp-content/themes/repmyblock/js/html5.js"></script>
+	<![endif]-->
+	
+<!-- This site is optimized with the Yoast SEO Premium plugin v7.7.3 - https://yoast.com/wordpress/plugins/seo/ -->
+<meta name="robots" content="noindex,follow"/>
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Rep My Block - Rep My Block" />
+<meta property="og:description" content="List of all the ressources that point to running for office in the 50 states." />
+<meta property="og:url" content="https://repmyblock.us" />
+<meta property="og:site_name" content="Rep My Block" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:description" content="Running for office in the United States" />
+<meta name="twitter:title" content="Rep My Block - Rep My Block" />
+<meta name="twitter:site" content="@RepMyBlock" />
+<meta name="twitter:image" content="https://static.repmyblock.nyc/pics/paste/PoliticalMachineKeepEngagementLow.jpg" />
+<meta name="twitter:creator" content="@RepMyBlock" />
+<script type='application/ld+json'>{"@context":"https:\/\/schema.org","@type":"WebSite","@id":"#website","url":"https:\/\/repmyblock.nyc\/","name":"Rep My Block","potentialAction":{"@type":"SearchAction","target":"https:\/\/repmyblock.nyc\/?s={search_term_string}","query-input":"required name=search_term_string"}}</script>
+<script type='application/ld+json'>{"@context":"https:\/\/schema.org","@type":"Organization","url":"https:\/\/repmyblock.nyc\/","sameAs":["https:\/\/www.facebook.com\/RepMyBlock","https:\/\/www.instagram.com\/repmyblocknyc","https:\/\/twitter.com\/RepMyBlock"],"@id":"https:\/\/repmyblock.nyc\/#organization","name":"Rep My Block","logo":"https:\/\/repmyblock.nyc\/wp-content\/uploads\/2019\/02\/RepMyBlock.png"}</script>
+<!-- / Yoast SEO Premium plugin. -->
+
+<link rel='dns-prefetch' href='https://fonts.googleapis.com/' />
+<link rel='dns-prefetch' href='https://s.w.org/' />
+<link href='https://fonts.gstatic.com' crossorigin rel='preconnect' />
+<link rel="alternate" type="application/rss+xml" title="Rep My Block &raquo; Feed" href="feed/" />
+<link rel="alternate" type="application/rss+xml" title="Rep My Block &raquo; Comments Feed" href="comments/feed/" />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/12.0.0-1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/12.0.0-1\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/repmyblock.nyc\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.2.1"}};
+			!function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55357,56424,55356,57342,8205,55358,56605,8205,55357,56424,55356,57340],[55357,56424,55356,57342,8203,55358,56605,8203,55357,56424,55356,57340]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+	<link rel='stylesheet' id='wp-block-library-css'  href='/css/style.min.css' type='text/css' media='all' />
+	<link rel='stylesheet' id='wp-block-library-theme-css'  href='/css/theme.min.css' type='text/css' media='all' />
+	<link rel='stylesheet' id='twentyfourteen-lato-css'  href='https://fonts.googleapis.com/css?family=Lato%3A300%2C400%2C700%2C900%2C300italic%2C400italic%2C700italic&#038;subset=latin%2Clatin-ext' type='text/css' media='all' />
+	<link rel='stylesheet' id='genericons-css'  href='/css/genericons.css' type='text/css' media='all' />
+	<link rel='stylesheet' id='twentyfourteen-style-css'  href='/css/style.css' type='text/css' media='all' />
+	<link rel='stylesheet' id='twentyfourteen-block-style-css'  href='/css/blocks.css' type='text/css' media='all' />
+<!--[if lt IE 9]>
+<link rel='stylesheet' id='twentyfourteen-ie-css'  href='https://repmyblock.nyc/wp-content/themes/repmyblock/css/ie.css?ver=20131205' type='text/css' media='all' />
+<![endif]-->
+<script type='text/javascript' src='/js/query.js'></script>
+<script type='text/javascript' src='/js/jquery-migrate.min.js'></script>
+<link rel='https://api.w.org/' href='wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="xmlrpc.php" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 5.2.1" />
+<link rel='shortlink' href='get-involved/' />
+<link rel="alternate" type="application/json+oembed" href="https://repmyblock.nyc/wp-json/oembed/1.0/embed?url=https%3A%2F%2Frepmyblock.nyc%2F" />
+		<style type="text/css" id="twentyfourteen-header-css">
+				.site-title,
+		.site-description {
+			clip: rect(1px 1px 1px 1px); /* IE7 */
+			clip: rect(1px, 1px, 1px, 1px);
+			position: absolute;
+		}
+				</style>
+		<link rel="icon" href="/pics/favico.png" sizes="32x32" />
+<link rel="icon" href="/pics/favico.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="/pics/favico.png" />
+<meta name="msapplication-TileImage" content="/pics/favico.png" />
+</head>
+
+<body class="home page-template-default page page-id-30 wp-embed-responsive header-image full-width grid">
+<div id="page" class="hfeed site">
+		<div class="header">
+	  <div class="header-left">
+			<a href="get-involved/" rel="home">
+				<img src="/pics/RepMyBlockLogo.png" width="99" height="71" alt="Rep My Block">
+			</a>
+			</div>
+		
+		<div class="header-right">
+  		<a href="https://repmyblock.nyc/contact.html" TARGET="ContactEmail" >CONTACT</a>
+		</div>
+	
+	</div>
+	
+	<header id="masthead" class="site-header" role="banner">
+		<div class="">
+
+
+	
+
+		<div id="search-container" class="search-box-wrapper hide">
+			<div class="search-box">
+				<form role="search" method="get" class="search-form" action="get-involved/">
+				<label>
+					<span class="screen-reader-text">Search for:</span>
+					<input type="search" class="search-field" placeholder="Search &hellip;" value="" name="s" />
+				</label>
+				<input type="submit" class="search-submit" value="Search" />
+			</form>			</div>
+		</div>
+	</header><!-- #masthead -->
+
+	<div id="main" class="site-main">
+
+<div id="main-content" class="main-content">
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			
+<article id="post-30" class="post-30 page type-page status-publish hentry">
+	<header class="entry-header"><h1 class="entry-title">Rep My Block</h1></header><!-- .entry-header -->
+	<div class="entry-content">
+		<p><!---
+
+
+<p class="BlueBox">
+<a href="interested" class="w3-button w3-bar-item w3-blue w3-hover-text-red BlueBox">ACT NOW! PETITIONING RUNS FROM
+FEBRUARY 26 TO APRIL 4, 2019.</a>
+--->
+
+
+<p class="BckGrndElement">LINKS TO SITE WITH INFORMATION</p>
+<div class="main">
+<strong>
+<p>
+	<TABLE>
+		
+		<TR>
+			<TH>County</TH>
+			<TH>State</TH>
+			<TH>Party</TH>
+			<TH>Group</TH>
+			<TH>Project Name</TH>
+			<TH>Website</TH>
+			<TH>Twitter</TH>
+		</TR>
+		
+				<TR>
+			<TD>Franklin</TD>
+			<TD>Ohio</TD>
+			<TD>Democrats</TD>
+			<TD>Franklin County Grassroots Democrats</TD>
+			<TD>Rep Your Block 2020</TD>
+			<TD><A HREF="https://RepYourBlock2020.org">https://repyourblock2020.org</A></TD>
+			<TD><A HREF="https://twitter.com/RepYourBlockFC">@RepYourBlockFC</A></TD>
+		</TR>
+	
+		<TR>
+			<TD>Hawai`i</TD>
+			<TD>Hawaii</TD>
+			<TD>Democrats</TD>
+			<TD>Democratic Party of Hawai`i</TD>
+			<TD>County Committee</TD>
+			<TD><A HREF="https://www.hawaiicountydemocrats.org">https://www.hawaiicountydemocrats.org</A></TD>
+			<TD>&nbsp;</TD>
+		</TR>
+		
+		
+		
+		<TR>
+			<TD>Kauai</TD>
+			<TD>Hawaii</TD>
+			<TD>Democrats</TD>
+			<TD>Democratic Party of Hawai`i</TD>
+			<TD>County Committee</TD>
+			<TD><A HREF="http://www.kauaidemocrats.org">http://www.kauaidemocrats.org</A></TD>
+			<TD>&nbsp;</TD>
+		</TR>
+
+		<TR>
+			<TD>Maui</TD>
+			<TD>Hawaii</TD>
+			<TD>Democrats</TD>
+			<TD>Democratic Party of Hawai`i</TD>
+			<TD>County Committee</TD>
+			<TD><A HREF="https://mauidemocrats.org/wp">https://mauidemocrats.org/wp</A></TD>
+			<TD>&nbsp;</TD>
+		</TR>
+
+		<TR>
+			<TD>Oahu</TD>
+			<TD>Hawaii</TD>
+			<TD>Democrats</TD>
+			<TD>Democratic Party of Hawai`i</TD>
+			<TD>County Committee</TD>
+			<TD><A HREF="https://www.oahudemocrats.org">https://www.oahudemocrats.org</A></TD>
+			<TD>&nbsp;</TD>
+		</TR>
+
+		<TR>
+			<TD>New York</TD>
+			<TD>New York</TD>
+			<TD>Democrats</TD>
+			<TD>Manhattan Young Democrats</TD>
+			<TD>Open Seat Project</TD>
+			<TD><A HREF="https://myd.nyc/open-seat-project">https://myd.nyc/open-seat-project</A></TD>
+			<TD><A HREF="https://twitter.com/gomyd">@gomyd</A></TD>
+		</TR>
+
+		<TR>
+			<TD>New York</TD>
+			<TD>New York</TD>
+			<TD>All</TD>
+			<TD>County Committee Sunlight Project, LLC.</TD>
+			<TD>Sunlight Project</TD>
+			<TD><A HREF="https://ccsunlight.org">https://ccsunlight.org</A></TD>
+			<TD><A HREF="https://twitter.com/ccsunlight">@ccsunlight</A></TD>
+		</TR>
+
+	<TR>
+			<TD>Kings</TD>
+			<TD>New York</TD>
+			<TD>All</TD>
+			<TD>RepMyBlock</TD>
+			<TD>Rep My Block</TD>
+			<TD><A HREF="https://RepMyBlock.NYC">https://repmyblock.nyc</A></TD>
+			<TD><A HREF="https://twitter.com/RepMyBlock">@RepMyBlock</A></TD>
+		</TR>
+
+		<TR>
+			<TD>Kings</TD>
+			<TD>New York</TD>
+			<TD>All</TD>
+			<TD>County Committee Sunlight Project, LLC.</TD>
+			<TD>Sunlight Project</TD>
+			<TD><A HREF="https://ccsunlight.org">https://ccsunlight.org</A></TD>
+			<TD><A HREF="https://twitter.com/ccsunlight">@ccsunlight</A></TD>
+		</TR> 
+		
+		<TR>
+			<TD>Kings</TD>
+			<TD>New York</TD>
+			<TD>Democrats</TD>
+			<TD>New Kings Democrats</TD>
+			<TD>Rep Your Block</TD>
+			<TD><A HREF="https://www.RepYourBlock.com">https://www.repyourblock.com</A></TD>
+			<TD><A HREF="https://twitter.com/RepYourBlock">@RepYourBlock</A></TD>
+		</TR>
+		
+		<TR>
+			<TD>Queens</TD>
+			<TD>New York</TD>
+			<TD>All</TD>
+			<TD>RepMyBlock</TD>
+			<TD>Rep My Block</TD>
+			<TD><A HREF="https://RepMyBlock.NYC">https://repmyblock.nyc</A></TD>
+			<TD><A HREF="https://twitter.com/RepMyBlock">@RepMyBlock</A></TD>
+		</TR>
+		
+		<TR>
+			<TD>Queens</TD>
+			<TD>New York</TD>
+			<TD>All</TD>
+			<TD>County Committee Sunlight Project, LLC.</TD>
+			<TD>Sunlight Project</TD>
+			<TD><A HREF="https://ccsunlight.org">https://ccsunlight.org</A></TD>
+			<TD><A HREF="https://twitter.com/ccsunlight">@ccsunlight</A></TD>
+		</TR>
+
+
+		<TR>
+			<TD>Queens</TD>
+			<TD>New York</TD>
+			<TD>Democrats</TD>
+			<TD>New Reformers</TD>
+			<TD>DL 72</TD>
+			<TD><A HREF="https://www.newreformers.org">https://www.newreformers.org</A></TD>
+			<TD><A HREF="https://twitter.com/ReformersNew">@ReformersNew</A></TD>
+		</TR>
+		
+		<TR>
+			<TD>Bronx</TD>
+			<TD>New York</TD>
+			<TD>All</TD>
+			<TD>RepMyBlock</TD>
+			<TD>Rep My Block</TD>
+			<TD><A HREF="https://RepMyBlock.NYC">https://repmyblock.nyc</A></TD>
+			<TD><A HREF="https://twitter.com/RepMyBlock">@RepMyBlock</A></TD>
+		</TR>
+
+
+		<TR>
+			<TD>Bronx</TD>
+			<TD>New York</TD>
+			<TD>All</TD>
+			<TD>County Committee Sunlight Project, LLC.</TD>
+			<TD>Sunlight Project</TD>
+			<TD><A HREF="https://ccsunlight.org">https://ccsunlight.org</A></TD>
+			<TD><A HREF="https://twitter.com/ccsunlight">@ccsunlight</A></TD>
+		</TR>
+
+
+		<TR>
+			<TD>Bronx</TD>
+			<TD>New York</TD>
+			<TD>Democrats</TD>
+			<TD>Bronx Progressives</TD>
+			<TD>County Committee</TD>
+			<TD><A HREF="http://www.bronxprogressives.org">ttp://www.bronxprogressives.org</A></TD>
+			<TD><A HREF="https://twitter.com/bxprogressives">@bxprogressives</A></TD>
+		</TR>
+		
+			
+		<TR>
+			<TD>Richmond</TD>
+			<TD>New York</TD>
+			<TD>All</TD>
+			<TD>RepMyBlock</TD>
+			<TD>Rep My Block</TD>
+			<TD><A HREF="https://RepMyBlock.NYC">https://repmyblock.nyc</A></TD>
+			<TD><A HREF="https://twitter.com/RepMyBlock">@RepMyBlock</A></TD>
+		</TR>
+
+
+		<TR>
+			<TD>Richmond</TD>
+			<TD>New York</TD>
+			<TD>All</TD>
+			<TD>County Committee Sunlight Project, LLC.</TD>
+			<TD>Sunlight Project</TD>
+			<TD><A HREF="https://ccsunlight.org">https://ccsunlight.org</A></TD>
+			<TD><A HREF="https://twitter.com/ccsunlight">@ccsunlight</A></TD>
+		</TR>
+		
+			<TR>
+			<TD>Richmond</TD>
+			<TD>New York</TD>
+			<TD>Democrats</TD>
+			<TD>Staten Island Women who March</TD>
+			<TD>County Committee</TD>
+			<TD><A HREF="https://www.siwomenwhomarch.org">https://www.siwomenwhomarch.org</A></TD>
+			<TD><A HREF="https://twitter.com/SIWomenWhoMarch">@SIWomenWhoMarch</A></TD>
+		</TR>
+
+		
+	</TABLE>
+	
+	 											
+	
+	
+	</p>
+	
+	
+	<p class="BckGrndElement">HOW DOES IT WORKS</p>
+<p><center><br />
+<iframe title="How to Work the Political Machine" width="474" height="267" src="https://www.youtube.com/embed/MnI7iBxCN4A?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br />
+</center></p>
+	
+	
+	</div><!-- .entry-content -->
+</article><!-- #post-30 -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+	</div><!-- #main-content -->
+
+<div id="secondary">
+	<h2 class="site-description">Rep My Block &#8211; Represent My Block &#8211; Nominate A Candidate</h2>
+	</div><!-- #secondary -->
+
+		</div><!-- #main -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+
+			
+			<div class="site-info footer">
+				<P CLASS="footerclass1">
+				<DIV CLASS="FooterTitle">REP MY BLOCK</DIV>
+				<DIV CLASS="FooterInfo">Represent Your Community By Running For County Committee</DIV>
+				</P>
+			<P CLASS="footerclass2">
+					<DIV CLASS="FooterSocial">
+					<A TARGET="twitter" CLASS="FooterSocial" HREF="https://twitter.com/RepMyBlock">Twitter</A>
+					<A TARGET="facebook" CLASS="FooterSocial" HREF="https://www.facebook.com/RepMyBlock">Facebook</A>
+					<A TARGET="instagram" CLASS="FooterSocial"  HREF="https://www.instagram.com/RepMyBlockNYC">Instagram</A>
+				</DIV>
+				<DIV CLASS="FooterStuff">
+					<I>RepMyBlock is a <A CLASS="FooterStuff" HREF="https://OutragedDems.NYC">Outraged Dems</A> project.</I>
+				</DIV>
+			</P>
+				
+				
+			
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120675447-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-120675447-3');
+</script>
+
+	<script type='text/javascript' src='wp-content/themes/repmyblock/js/functions.js%3Fver=20150315'></script>
+<script type='text/javascript' src='wp-includes/js/wp-embed.min.js%3Fver=5.2.1'></script>
+</body>
+</html>
